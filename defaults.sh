88885b75ed772fea1daeeb0b331031dabfcef135
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-03 21:35:23 -0700

    Don't have Ouroboros default to running.

--- defaults.sh
+++ defaults.sh
@@ -7,7 +7,7 @@
 # service_name=
 
 # Leave blank to disable this service by default.
-set_service_flag $service_name yes
+set_service_flag $service_name
 #set_service_flag $service_name yes
 
 # Sets the application prefix depending on what $INGRESS_TYPE is set to. 
